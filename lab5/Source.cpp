5d943829db6df9269fc15e5302555fd7e188ff9a
Sirisap Siripattanakul <sirisapsiripattanakul@outlook.com>
2020-10-20 14:33:26 +0700

    update star collision

--- lab5/Source.cpp
+++ lab5/Source.cpp
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <windows.h>
 #include <conio.h>
+#include <time.h>
+#include <math.h>
 
 void gotoxy(int, int);
 void setcursor(bool);
@@ -18,6 +20,13 @@ void bullet_movement();
 bool check_wall_bullet(int, int);
 int choose_empty_bullet_slot();
 
+void draw_star(int, int);
+void erase_star(int, int);
+void update_stars();
+int* random_star_position();
+bool check_star_duplicate_position(int*, int);
+bool check_bullet_star_collision(int, int);
+
 /*------------- game config--------------- */
 
 // default config
@@ -30,6 +39,10 @@ const int SHIP_COLOR[2] = { 2, 4 };
 // bullet config
 const int BULLET_COLOR[2] = { 6, 6 };
 const int BULLETS_SIZE = 5;
+
+// star config
+const int STAR_COLOR[2] = { 6, 0 };
+const int STARS_NUMBER = 20;
 
 /*-------------- gobal variables --------------*/
 
@@ -49,9 +62,18 @@ typedef struct Bullet {
 Bullet bullets[BULLETS_SIZE];
 int bullets_capacity = 0;
 
+typedef struct Star {
+	int x;
+	int y;
+	bool is_star_alive;
+} Star;
+Star stars[STARS_NUMBER];
 
 int main() {
 	/* initial state */
+
+	// set a seed
+	srand(time(NULL));
 
 	// initialize ship
 	setcolor(SHIP_COLOR[0], SHIP_COLOR[1]);
@@ -63,6 +85,26 @@ int main() {
 		bullets[i].y = 0;
 		bullets[i].is_bullet_activate = false;
 	}
+	// initialize stars
+	for (int i = 0; i < STARS_NUMBER; i++) {
+		bool duplicate = true;
+		int* coord = new int[2];
+		while (duplicate) {
+			coord = random_star_position();
+			if (!check_star_duplicate_position(coord, i)) {
+				duplicate = false;
+			}
+		}
+		stars[i].x = coord[0];
+		stars[i].y = coord[1];
+		stars[i].is_star_alive = false;
+	}
+	for (int i = 0; i < STARS_NUMBER; i++) {
+		if (!stars[i].is_star_alive) {
+			draw_star(stars[i].x, stars[i].y);
+			stars[i].is_star_alive = true;
+		}
+	}
 
 	/* game loop  */
 
@@ -70,6 +112,7 @@ int main() {
 		check_input();
 		ship_movement();
 		bullet_movement();
+		update_stars();
 		Sleep(GAME_SPEED);
 	} while (ch != 'x');
 	return 0;
@@ -187,14 +230,21 @@ void bullet_movement() {
 	for (int i = 0; i < BULLETS_SIZE; i++) {
 		if (bullets[i].is_bullet_activate) {
 			erase_bullet(bullets[i].x, bullets[i].y);
-			if (!check_wall_bullet(bullets[i].x, bullets[i].y)) {
+			if (check_bullet_star_collision(bullets[i].x, bullets[i].y)) { // check star collision
+			bullets[i].is_bullet_activate = false;
+			bullets_capacity = bullets_capacity - 1;
+			Beep(700, 100);
+			erase_bullet(bullets[i].x, bullets[i].y);
+			}
+			else if (!check_wall_bullet(bullets[i].x, bullets[i].y)) {
 				draw_bullet(bullets[i].x, --bullets[i].y);
 			}
 			else if (check_wall_bullet(bullets[i].x, bullets[i].y)) {
 				bullets[i].is_bullet_activate = false;
 				bullets_capacity = bullets_capacity - 1;
 				erase_bullet(bullets[i].x, bullets[i].y);
-			}
+			} 
+
 		}
 	}
 }
@@ -212,4 +262,71 @@ int choose_empty_bullet_slot() {
 	}
 	return -1; // never reach!
 }
+
+// star functions
+
+int* random_star_position() {
+	int* coord = new int[2];
+	coord[0] = round(((double)rand() / (double)RAND_MAX) * 60 + 10);
+	coord[1] = round(((double)rand() / (double)RAND_MAX) * 3 + 2);
+	return coord;
+}
+
+bool check_star_duplicate_position(int* coord, int current_position) {
+	for (int i = 0; i < STARS_NUMBER; i++) {
+		if (i == current_position)
+			continue;
+		if (coord[0] == stars[i].x && coord[1] == stars[i].y)
+			return true;
+	}
+	return false;
+}
+
+void draw_star(int x, int y) {
+	setcolor(STAR_COLOR[0], STAR_COLOR[1]);
+	gotoxy(x, y);
+	printf("*");
+	setcolor(DEFAULT_COLOR[0], DEFAULT_COLOR[1]);
+}
+
+void erase_star(int x, int y) {
+	gotoxy(x, y);
+	setcolor(DEFAULT_COLOR[0], DEFAULT_COLOR[1]);
+	printf(" ");
+}
+
+void update_stars() {
+	for (int i = 0; i < STARS_NUMBER; i++) {
+		if (!stars[i].is_star_alive) {
+			bool duplicate = true;
+			int* coord = new int[2];
+			while (duplicate) {
+				coord = random_star_position();
+				if (!check_star_duplicate_position(coord, i)) {
+					duplicate = false;
+				}
+			}
+			stars[i].x = coord[0];
+			stars[i].y = coord[1];
+			stars[i].is_star_alive = true;
+			draw_star(stars[i].x, stars[i].y);
+		}
+	}
+}
+
+// collision check
+
+bool check_bullet_star_collision(int x, int y) {
+		for (int j = 0; j < STARS_NUMBER; j++) {
+			if (stars[j].is_star_alive &&
+				x == stars[j].x &&
+				y == stars[j].y) {
+				stars[j].is_star_alive = false;
+				return true;
+			}
+		}
+	return false;
+}
+
+
 
